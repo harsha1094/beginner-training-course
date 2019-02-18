99e38bf8ec04fbc50e5e31fc02ca546b31776f7d
harsha1094 <harsha1094@gmail.com>
2019-02-18 21:58:06 +0530

    Update Program with RETURN FUNCTION

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,51 +11,52 @@ namespace Calculator
             a=Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter Second Number: ");
             b=Convert.ToDouble(Console.ReadLine());
-            sum(a,b);
-            sub(a,b);
-            div(a,b);
-            mod(a,b);
-            mul(a,b);
+            Console.WriteLine("The Addition is {0}",sum(a,b));
+            Console.WriteLine("The Subtraction is {0}",sub(a,b));
+            Console.WriteLine("The Division is {0}",div(a,b));
+            Console.WriteLine("The Modulo is {0}",mod(a,b));
+            Console.WriteLine("The Multiplication is {0}",mul(a,b));
         }
 /*
 ? ADDITION
  */
-        static void sum(double a,double b)
+        static double sum(double a,double b)
         {
             double c=a+b;
-            Console.WriteLine("The Summation is {0}",c);
+            return c;
         }
+
 /*
 ? SUBTRACTION
  */
-        static void sub(double a,double b)
+        static double sub(double a,double b)
         {
             double c=a-b;
-            Console.WriteLine("The Subtraction is {0}",c);
+            return c;
         }
 /*
 ? DIVISION
  */
-        static void div(double a,double b)
+        static double div(double a,double b)
         {
             double c=a/b;
-            Console.WriteLine("The Division is {0}",c);
+            return c;
         }
 /*
 ? MODULO
  */
-        static void mod(double a,double b)
+        static double mod(double a,double b)
         {
             double c=a%b;
-            Console.WriteLine("The Modulo is {0}",c);
+            return c;
         }
 /*
 ? MULTIPLICATION
  */
-        static void mul(double a,double b)
+        static double mul(double a,double b)
         {
             double c=a*b;
-            Console.WriteLine("The Multiplication is {0}",c);
+            return c;
         }
     }
 }
