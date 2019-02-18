a92d1a5e2326db69fb75aaa045a1a853b201a705
harsha1094 <harsha1094@gmail.com>
2019-02-18 09:37:08 +0530

    pre-final

---
+++ Problematic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problematic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int skip=1;
+            List<int> Range = new List<int>();
+                for(int i=0;i<=5*skip;i=i+skip)
+                    {
+                        if (i==0){
+                            Range.Add(1);
+                            }
+                        else{
+                            Range.Add();
+                            }
+                    }
+            Console.WriteLine(Range);
+        }
+    }
+}
